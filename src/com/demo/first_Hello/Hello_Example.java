5b51f15fabd1915b2d95d216e44579b474b8035f
kumargulshan28 <kumargulshan9128@gmail.com>
2018-06-29 15:47:30 +0530

    2nd time trying to add something on exiting method

--- src/com/demo/first_Hello/Hello_Example.java
+++ src/com/demo/first_Hello/Hello_Example.java
@@ -6,9 +6,13 @@ public class Hello_Example {
 		System.out.println("The name which given by the man method:-"+name);
 	}
 	
+	public void displayName(String first_Name, String last_Name) {
+		System.out.println("The name which is given by the Main method:-"+first_Name+" "+last_Name);
+	}
 	public static void main(String[] args) {
 		Hello_Example h1= new Hello_Example();
-		h1.displayName("Gulshan");;
+		h1.displayName("Gulshan");
+		h1.displayName("Gulshan", "Kumar");;
 	}
 
 }
