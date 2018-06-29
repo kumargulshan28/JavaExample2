024032c63349f9a4efc8ae58da4abaa2f910fbf2
kumargulshan28 <kumargulshan9128@gmail.com>
2018-06-29 16:05:50 +0530

    3rd time

--- src/com/demo/first_Hello/Hello_Example.java
+++ src/com/demo/first_Hello/Hello_Example.java
@@ -1,5 +1,7 @@
 package com.demo.first_Hello;
 
+import java.util.Date;
+
 public class Hello_Example {
 	
 	public void displayName(String name) {
@@ -8,6 +10,15 @@ public class Hello_Example {
 	
 	public void displayName(String first_Name, String last_Name) {
 		System.out.println("The name which is given by the Main method:-"+first_Name+" "+last_Name);
+	}
+	
+	public void displayAge(String dob) {
+		int x=0;
+	String[] s=dob.split(dob, x);
+	 int year=2018-x;
+	 String year1=String.valueOf(year);
+			 
+		System.out.println("His age is:-"+year1);
 	}
 	public static void main(String[] args) {
 		Hello_Example h1= new Hello_Example();
